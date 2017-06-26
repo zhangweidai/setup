c62bac06226685ffcac9f69df0bf7a64e9bc5884
Peter Zhang <peter_zhang@mentor.com>
2017-06-26 00:03:41 -0700

    hang.py

--- python/hang.py
+++ python/hang.py
@@ -1,30 +1,60 @@
 #!/usr/bin/python
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from os import curdir, sep
 import cgi
-import urlparse
-import pickle
 import random
 
-def setup():
-    words = ["rabbit", "bunny", "carrot"]
+MAX_MISSES = 2
+LOST_RECORD = '|'
+
+print "!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+#class GameInfo():
 
 
-words = ["rabbit", "bunny", "carrot"]
-def application(env, start_response):
-    global words
-    start_response('200 OK', [('Content-Type','text/html')])
-    return words[0]
 
-PORT_NUMBER = 8080
 guesses = {}
 guesses["default"] = ""
 game = {}
-game["default"] = random.choice(words)
 letters = "abcdefghijklmnopqrstuvwyz"
 cnextLetter = 0
 
-def pretty(word):
+words = ["rabbit", "bunny", "carrot"]
+game["default"] = random.choice(words)
+
+print words
+
+def appendToGuesses(gameId, state, currentCount):
+    global guesses
+
+    if currentCount > len(guesses[gameId]):
+        guesses[gameId] += state
+    else:
+        newGuess = guesses[gameId][:currentCount]+state
+        print "newGuess"
+        print newGuess
+        guesses[gameId] = newGuess
+        currentCount += 1
+    return currentCount
+
+def getCurrentGuesses(guessesSoFar):
+    if not LOST_RECORD in guessesSoFar:
+        return guessesSoFar
+    index = guessesSoFar.rfind(LOST_RECORD)
+    what =  guessesSoFar[index+1:]
+    return what
+
+
+
+def countIncorrects(underscoredWord, guessesSoFar):
+    missed = 0
+    currentGuesses = getCurrentGuesses(guessesSoFar)
+    for guess in guessesSoFar:
+        if guess not in underscoredWord:
+            missed += 1
+    return missed
+
+
+def spaced(word):
     ret = ""
     for letter in word:
         ret += letter + " "
@@ -56,6 +86,8 @@ def nextLetter(usedLetters):
     return ret
 
 def showOnly(namestr, currentCount):
+    print "show only"
+    print currentCount
     return namestr[:currentCount]
 
 def parseParse(string):
@@ -69,119 +101,164 @@ def parseParse(string):
             print "got this instead {} {}".format(len(keys), keys)
     return ret
 
-#This class will handles any incoming request from
-#the browser 
-class myHandler(BaseHTTPRequestHandler):
-    #Handler for the GET requests
-    def do_GET(self):
-        if self.path.startswith("/favicon.ico"):
-            return
-        currentCount = 0
-        namestr = "default"
+def writePage(gameId, currentCount, letter):
+    guessesSoFar = showOnly(guesses[gameId], currentCount)
 
-        parsed_path = urlparse.urlparse(self.path)
-        myDic = parseParse(parsed_path.query)
-        if len(myDic) > 0:
-            currentCount = int(myDic["count"])
-            namestr = myDic["id"]
+    print "guessesSoFar" + guessesSoFar
+    currentGuesses = getCurrentGuesses(guessesSoFar)
+    print "currentGuesses" + currentGuesses
 
-        self.writePage(namestr, currentCount)
+    word = game[gameId]
+    underscoredWord = underscored(currentGuesses, word)
 
+    msg = ""
+    if (underscoredWord == word):
+       msg += '<h1>Win</h1>'
 
-    #Handler for the POST requests
-    def do_POST(self):
-        global guesses, game
-        global cnextLetter
-        currentCount = 0
-        namestr = "default"
+    cheatBtn = '\
+<form method="POST" action="/cheat">\
+<input type="submit" value="Cheat"/></form>'\
 
-        form = None
-        letter = ""
-        if "cheat" in self.path:
-            letter = nextLetterFrom(game[namestr])
-            
-        if "newgame" in self.path:
-            print "newgame"
-            cnextLetter = 0
-            game[namestr] = random.choice(words)
-            guesses[namestr] = ""
+    newGameBtn = '\
+<form method="POST" action="/newgame">\
+<input type="submit" value="Start Over"/></form>'\
 
-        if "guess" in self.path:
-            form = cgi.FieldStorage(fp=self.rfile, 
-                headers=self.headers,
-                environ={'REQUEST_METHOD':'POST',
-                'CONTENT_TYPE':self.headers['Content-Type'],
-            })
+    undoBtn = '\
+<form method="POST" action="/guess?id={}?count={}">\
+<input type="submit" value="undo"/ {}></form>'.format(
+        gameId, 
+        currentCount-1,
+        "disabled" if currentCount < 1 else "")
 
-            if form.has_key("letter"):
-                letter = form["letter"].value
+    redoBtn = '\
+<form method="POST" action="/guess?id={}?count={}">\
+<input type="submit" value="redo"/ {}></form>'.format(
+        gameId, 
+        currentCount+1,
+        "" if currentCount < len(guesses[gameId]) else "disabled")
 
-        #if "?" in self.path or "cheat" in self.path:
-        parsed_path = urlparse.urlparse(self.path)
-        print "parsed_path"
-        print parsed_path
-        myDic = parseParse(parsed_path.query)
-        print myDic
-        if not len(myDic) == 0:
-            currentCount = int(myDic["count"])
-            namestr = myDic["id"]
-        else :
-            currentCount = len(guesses[namestr])
+    increment = 0
+    if letter:
+        increment = 1
 
-        guesses[namestr] += letter
+    guess = '\
+<form method="POST" action="/guess?id={}?count={}">\
+<input type="text" name="letter"/ autocomplete="off">\
+<input type="submit" value="guess"/></form>'.format(
+        gameId, 
+        currentCount + increment)
 
-        currentCount += 1
-        self.writePage(namestr, currentCount)
-        return
+    incorrect = countIncorrects(underscoredWord, currentGuesses)
 
-
-    def writePage(self, namestr, currentCount):
-        self.send_response(200)
-        self.end_headers()
-        guessesSoFar = showOnly(guesses[namestr], currentCount)
-        word = game[namestr]
-        vword = underscored(guessesSoFar, word)
-        win = ""
-        if (vword == word):
-            win = '<h1>Win</h1>\
-</form>'
-
-        html = '\
-<html>\
+    msg += '\
 <h3> game is for : {} </h3>\
 <h3> word is: {} </h3>\
 <h3> user visible word is: {} </h3>\
-<h3> guesses so far {} </h3>\
-<form method="POST" action="/cheat">\
-<input type="submit" value="cheat"/>\
-</form>\
-<form method="POST" action="/newgame">\
-<input type="submit" value="newgame"/>\
-</form>\
-<form method="POST" action="/guess?id={}?count={}">\
-<input type="text" name="letter"/>\
-<input type="submit" value="guess"/>\
-{}\
-</form>'.format(
-        namestr,
+<h3> current guessed letters : {} </h3>\
+<h3> guessed letters total : {} </h3>\
+<h3> incorrect guesses: {} </h3>\
+'.format(
+        gameId,
         word, 
-        pretty(vword), 
-        guessesSoFar, 
-        namestr, 
-        currentCount, 
-        win)
+        spaced(underscoredWord), 
+        currentGuesses,
+        guessesSoFar,
+        str(incorrect)
+        ) 
 
-        self.wfile.write(html)
+    if incorrect >= MAX_MISSES:
+        nextGameBtn = '\
+<form method="POST" action="/nextgame?id={}?count={}">\
+<input type="submit" value="Next Game"/></form>'.format(
+        gameId, 
+        currentCount + 1)
 
-#try:
-#    #Create a web server and define the handler to manage the
-#    #incoming request
-#    server = HTTPServer(('', PORT_NUMBER), myHandler)
-#    print 'Started httpserver on port ' , PORT_NUMBER
-#    	
-#    #Wait forever for incoming htto requests
-#    server.serve_forever()
-#
-#except KeyboardInterrupt:
-#    print '^C received, shutting down the web server'
-#    server.socket.close()
+        msg += "<h2>Game Lost</h2>"
+        msg += nextGameBtn
+    else:
+        msg += cheatBtn
+        msg += undoBtn
+        msg += redoBtn
+        msg += guess
+    msg += newGameBtn
+    
+    return '<html> {} </html>'.format(msg)
+
+
+#Handler for the POST requests
+def getHtml(env):
+    query = env['QUERY_STRING']
+    path = env['PATH_INFO']
+
+    if "favicon" in path:
+        return ""
+    print "query"
+    print query
+    print "path"
+    print path
+
+    global guesses, game
+    global cnextLetter
+
+    currentCount = 1
+    gameId = "default"
+
+    form = None
+    letter = ""
+    noGuess = False
+    if "cheat" in path:
+        letter = nextLetterFrom(game[gameId])
+        print letter
+        
+    if "newgame" in path:
+        print "newgame"
+        cnextLetter = 0
+        game[gameId] = random.choice(words)
+        guesses[gameId] = ""
+
+    if "guess" in path:
+        input = env['wsgi.input']
+        form = cgi.FieldStorage(
+                fp=input,
+                environ=env)
+
+        if form.has_key("letter"):
+            letter = form["letter"].value
+        else:
+            noGuess = True
+
+
+    #if "?" in self.path or "cheat" in self.path:
+    if query:
+        myDic = parseParse(query)
+
+        if not len(myDic) == 0:
+            print "fromA"
+            currentCount = int(myDic["count"])
+            gameId = myDic["id"]
+        else :
+            print "fromB"
+            currentCount = len(guesses[gameId])
+
+    if "nextgame" in path:
+        print "currentCount"
+        print currentCount
+        currentCount = appendToGuesses(gameId, LOST_RECORD, currentCount)
+
+        print "currentCount"
+        print currentCount
+        return writePage(gameId, currentCount, '')
+
+
+    if letter:
+        print "this is my letter : "  + letter
+        print "this is my currentCount : " + str(currentCount)
+        print "this is my len  of guesses: " + str(len(guesses[gameId]))
+        currentCount = appendToGuesses(gameId, letter, currentCount)
+
+    return writePage(gameId, currentCount, letter)
+
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    html = getHtml(env)
+    return html
