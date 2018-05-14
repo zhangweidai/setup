26846a9c0fa483c7ab4469b20ccd0009bbfdb5eb
Peter Zhang <peter_zhang@mentor.com>
2018-05-14 16:37:37 -0700

    missing setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 # setup
 sudo apt-get install xclip
 sudo apt-get install silversearcher-ag
+sudo apt install exuberant-ctags
+sudo apt install vim
 mkdir $HOME/.undo
 cd $HOME
 ln -sf ./setup/.bashrc
 ln -sf ./setup/.bash_aliases
 ln -sf ./setup/.vim
+ln -sf ./setup/.vimrc
 ln -sf ./setup/.gitconfig
