27422dbd7e16c8f06235a6075f124e3145377774
Peter Zhang <peter_zhang@mentor.com>
2020-08-23 10:39:19 -0700

    f

---
+++ setup2.sh
@@ -0,0 +1,11 @@
+# sudo apt install wsl
+# mkdir ~/.vim_mru_files
+# sudo update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
+# sudo apt install python3-pip
+pip install Panda3D
+pip install screeninfo
+pip install psd-tools3
+pip install Pillow
+pip install hurry.filesize
+pip install blender
+pip install imageio
